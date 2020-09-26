25c13c7e0b2a05e507a4e476e8dd5677fa53f88b
Tanopwan Wongpichitkun <tanopwan.won@ascendcorp.com>
2020-09-26 16:39:42 +0700

    remove addMonths to get consequentive months

--- src/js/datePicker/templates/months.js
+++ src/js/datePicker/templates/months.js
@@ -5,16 +5,9 @@ import {
 } from 'date-fns';
 
 export default (data) => {
-  return `${data.months.map((d, i) => {
-    console.log(d)
-    console.log(typeof(d))
-    console.log(parse(d,'MM', new Date()))
-    let dObj = parse(d,'MM', new Date())
-    console.log(addMonths(dObj, i))
-    return `<div class="datepicker-month" data-month="${format(addMonths(dObj, i), 'MM', {
-      locale: data.locale
-    })}">${format(addMonths(dObj, i), 'MMM', {
-      locale: data.locale
-    })}</div>`
-  }).join('')}`;
+  return `${data.months.map((d, i) => `<div class="datepicker-month" data-month="${format(parse(d, 'MM', new Date(), i), 'MM', {
+    locale: data.locale
+  })}">${format(parse(d, 'MM', new Date(), i), 'MMM', {
+    locale: data.locale
+  })}</div>`).join('')}`;
 }
